0c33081c8ea7d01b4a70d71543064415da9b7ec5
Maikel Veen <veenmaikel@gmail.com>
2019-02-06 19:08:25 +0100

    Get project setup

---
+++ NanoleafAmbilight/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+using NanoleafAmbilight.ScreenCapture;
+
+namespace NanoleafAmbilight
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Test code for scaling down of bitmap
+            Bitmap test = Bitmap.FromFile("C:/Users/Maikel/AppData/Roaming/.minecraft/screenshots/2019-02-04_13.45.25.png") as Bitmap;
+            Bitmap newB = BitmapScaler.ScaleBitmapDown(test);
+            
+            newB.Save("test.jpg");
+        }
+    }
+}
--- NanoleafAmbilight/ScreenCapture/ScreenDuplicater.cs
+++ NanoleafAmbilight/ScreenCapture/ScreenDuplicater.cs
@@ -1,7 +1,27 @@
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using SharpDX;
+using SharpDX.Direct3D11;
+using SharpDX.DXGI;
+using Device = SharpDX.Direct3D11.Device;
+using MapFlags = SharpDX.Direct3D11.MapFlags;
+
+
 namespace NanoleafAmbilight.ScreenCapture
 {
     public class ScreenDuplicater
     {
-        
+        public static Bitmap GetCurrentScreen(int adapterNumber = 0, int output = 0)
+        {
+            Factory1 factory = new Factory1();
+            Adapter adapter = factory.GetAdapter1(adapterNumber);
+
+            Device device = new Device(adapter);
+            
+
+            throw new NotImplementedException();
+        }
     }
 }
