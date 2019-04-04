83590739bed95fd2eb48046b4be37ea02b6ab151
Maikel Veen <veenmaikel@gmail.com>
2019-04-04 23:11:25 +0200

    Add test images

--- NanoleafAmbilight/Program.cs
+++ NanoleafAmbilight/Program.cs
@@ -12,11 +12,24 @@ namespace NanoleafAmbilight
     {
         public static void Main(string[] args)
         {
-            Stopwatch stopwatch = new Stopwatch();
-            HSBColor color = new HSBColor(39, 99, 99);
+            /*Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
 
+            Bitmap loadedBitmap =
+                Image.FromFile("C:/Users/Maikel/RiderProjects/NanoleafDesktopAmbilight/NanoleafAmbilight/distinctColorTest2.png") as Bitmap;
+
+            ColorHistogram colorHistogram = new ColorHistogram(loadedBitmap.GetBitmapColors());
+            Console.WriteLine(colorHistogram.);
+            stopwatch.Stop();
+            Console.WriteLine($"Milliseconds elapsed: {stopwatch.ElapsedMilliseconds}");*/
+
+            //string token = NanoleafClient.GetAuthToken("http://192.168.192.50:16021");
+            Stopwatch stopwatch = new Stopwatch();
+            HSBColor color = new HSBColor(39,99,99);
+            
             NanoleafClient nanoleafClient = new NanoleafClient("http://192.168.192.50:16021");
             nanoleafClient.Start();
+           
         }
     }
 }
